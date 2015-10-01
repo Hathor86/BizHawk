6301cd4e8dceb93d871372f3f3586b1868067e00
Hathor86 <francois.guiot@gmail.com>
2015-10-01 23:43:14 +0200

    The basics
    Add ICustomGameTolls interface and few lines of codes.
    The basics works, I can load a form from a foreign dll.

---
+++ BizHawk.Client.EmuHawk/tools/ICustomGameTool.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.Client.EmuHawk
+{
+    /// <summary>
+    /// Interface to implements in order to make a custom tool for a specific game
+    /// </summary>
+    public interface ICustomGameTool : IToolForm
+    {
+        /// <summary>
+        /// Load the custom tool
+        /// </summary>
+        /// <returns>True if load is successfull otherwise, false</returns>
+        bool Load();
+    }
+}
